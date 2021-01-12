9e2b6c71f567342d0b9055b52aac47308a38a7e9
piale <32018252+piale@users.noreply.github.com>
2021-01-13 00:44:35 +0100

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-apt update -y
-apt upgrade -y
-apt install docker.io -y
+PODS_SUBNET=10.10.0.0/16
+K8S_VERSION=1.19.6-00
+K8S_VERSION_SHORT=1.19.6
+CONTROL_PLANE_ENDPOINT=master:6443
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
+sudo sed -i 's|/swap.img|#/swap.img|' /etc/fstab
+sudo swapoff -a
 
-apt update -y && apt install -y apt-transport-https curl
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install docker.io -y
+
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-apt update -y
-apt install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update -y
+sudo apt-get install -y kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION
+sudo apt-mark hold kubelet kubeadm kubectl
 
-kubeadm init
+cat <<EOF | sudo tee kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+kubernetesVersion: $K8S_VERSION_SHORT
+controlPlaneEndpoint: "$CONTROL_PLANE_ENDPOINT"
+networking:
+  podSubnet: $PODS_SUBNET
+EOF
+
+sudo kubeadm init --config=kubeadm-config.yaml --upload-certs | sudo tee kubeadm-init.out
 
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo 'source <(kubectl completion bash)' >> $HOME/.bashrc
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
-kubectl apply -f calico.yaml
+wget https://docs.projectcalico.org/manifests/calico.yaml
 
-rm -f calico.yaml
+sudo sed -zi 's|            # - name: CALICO_IPV4POOL_CIDR\n            #   value: "192.168.0.0/16"|            - name: CALICO_IPV4POOL_CIDR\n              value: '"$PODS_SUBNET"'|' calico.yaml
+kubectl apply -f calico.yaml
